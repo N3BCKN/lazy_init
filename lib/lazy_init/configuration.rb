7aee0d60e4aad9ae78f5482cfb4507580e152e2b
H3BCKN <ka.koszewski@gmail.com>
2025-07-03 21:17:40 +0200

    get rid of unsued configs

--- lib/lazy_init/configuration.rb
+++ lib/lazy_init/configuration.rb
@@ -3,28 +3,18 @@
 module LazyInit
   # Global configuration for LazyInit gem behavior.
   #
-  # Provides centralized configuration for debugging, performance tracking,
-  # timeout defaults, and memory management settings.
+  # Provides centralized configuration for timeout defaults and memory management settings.
   #
   # @example Basic configuration
   #   LazyInit.configure do |config|
-  #     config.debug = true
   #     config.default_timeout = 30
+  #     config.max_lazy_once_entries = 5000
   #   end
   #
   # @since 0.1.0
   class Configuration
-    # @!attribute [rw] debug
-    #   @return [Boolean] enable debug logging (default: false)
-
     # @!attribute [rw] default_timeout
     #   @return [Numeric, nil] default timeout in seconds for all lazy attributes (default: nil)
-
-    # @!attribute [rw] track_performance
-    #   @return [Boolean] enable performance tracking (default: false)
-
-    # @!attribute [rw] enable_warnings
-    #   @return [Boolean] enable warning messages (default: true)
 
     # @!attribute [rw] max_lazy_once_entries
     #   @return [Integer] maximum entries in lazy_once cache (default: 1000)
@@ -32,15 +22,11 @@ module LazyInit
     # @!attribute [rw] lazy_once_ttl
     #   @return [Numeric, nil] time-to-live for lazy_once entries in seconds (default: nil)
 
-    attr_accessor :debug, :default_timeout, :track_performance,
-                  :enable_warnings, :max_lazy_once_entries, :lazy_once_ttl
+    attr_accessor :default_timeout, :max_lazy_once_entries, :lazy_once_ttl
 
     # Initializes configuration with default values.
     def initialize
-      @debug = false
       @default_timeout = nil
-      @track_performance = false
-      @enable_warnings = true
       @max_lazy_once_entries = 1000
       @lazy_once_ttl = nil
     end
@@ -60,9 +46,9 @@ module LazyInit
   #
   # @example Environment-specific configuration
   #   LazyInit.configure do |config|
-  #     config.debug = Rails.env.development?
   #     config.default_timeout = 10
   #     config.max_lazy_once_entries = 5000
+  #     config.lazy_once_ttl = 1.hour
   #   end
   def self.configure
     yield(configuration)
--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -8,24 +8,24 @@ RSpec.describe LazyInit::Configuration do
 
   it 'has default values' do
     config = LazyInit::Configuration.new
-    
-    expect(config.debug).to be false
+
+    # expect(config.debug).to be false
     expect(config.default_timeout).to be_nil
-    expect(config.track_performance).to be false
-    expect(config.enable_warnings).to be true
+    # expect(config.track_performance).to be false
+    # expect(config.enable_warnings).to be true
     expect(config.max_lazy_once_entries).to eq(1000)
     expect(config.lazy_once_ttl).to be_nil
   end
 
   it 'allows configuration through block' do
     LazyInit.configure do |config|
-      config.debug = true
+      # config.debug = true
       config.default_timeout = 30
       config.max_lazy_once_entries = 500
     end
-    
+
     config = LazyInit.configuration
-    expect(config.debug).to be true
+    # expect(config.debug).to be true
     expect(config.default_timeout).to eq(30)
     expect(config.max_lazy_once_entries).to eq(500)
   end
@@ -34,18 +34,18 @@ RSpec.describe LazyInit::Configuration do
     LazyInit.configure do |config|
       config.default_timeout = 5
     end
-    
+
     test_class = Class.new do
       extend LazyInit
-      
+
       lazy_attr_reader :slow_operation do
         sleep(0.1)
         'result'
       end
     end
-    
+
     instance = test_class.new
-    
+
     # Should use configured timeout
     expect(instance.slow_operation).to eq('result')
   end
@@ -54,10 +54,10 @@ RSpec.describe LazyInit::Configuration do
     LazyInit.configure do |config|
       config.max_lazy_once_entries = 3
     end
-    
+
     test_class = Class.new { include LazyInit }
     instance = test_class.new
-    
+
     # Create more entries than configured max
     5.times do |i|
       instance.define_singleton_method("method_#{i}") do
@@ -65,7 +65,7 @@ RSpec.describe LazyInit::Configuration do
       end
       instance.send("method_#{i}")
     end
-    
+
     stats = instance.lazy_once_statistics
     expect(stats[:total_entries]).to be <= 3
   end
